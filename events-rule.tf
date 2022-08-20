746a35b407c13e1504d767690c3cc11a9598e129
matheusferro <matheussouzaferro@gmail.com>
2022-08-20 18:33:39 -0300

    running aws lambda each 1 minute with cloudwatch rule

--- events-rule.tf
+++ events-rule.tf
@@ -23,21 +23,21 @@
 # }
 
 resource "aws_cloudwatch_event_rule" "cloudwatch_lambda_rule" {
-    name = "cloudwatch-lambda-rule"
-    description = "Call lambda every minute"
-    schedule_expression = "rate(1 minute)"
+  name                = "cloudwatch_lambda_rule"
+  description         = "Call lambda every minute"
+  schedule_expression = "rate(1 minute)"
 }
 
 resource "aws_cloudwatch_event_target" "call_lambda" {
-    rule = aws_cloudwatch_event_rule.cloudwatch_lambda_rule.name
-    arn = aws_lambda_function.hello_lambda.arn
-    input = jsonencode({"job": "cloudwatch-rules"})
+  rule  = aws_cloudwatch_event_rule.cloudwatch_lambda_rule.name
+  arn   = aws_lambda_function.hello_lambda.arn
+  input = jsonencode({ "job" : "cloudwatch-rules" })
 }
 
 resource "aws_lambda_permission" "cloudwatch_invoke" {
- statement_id  = "AllowExecutionFromCloudWatch"
- action        = "lambda:InvokeFunction"
- function_name = aws_lambda_function.hello_lambda.function_name
- principal     = "events.amazonaws.com"
- source_arn    = aws_cloudwatch_event_rule.cloudwatch_lambda_rule.arn
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.cloudwatch_lambda_rule.arn
 }
--- lambda.tf
+++ lambda.tf
@@ -1,49 +1,96 @@
-data "archive_file" "python_lambda_package" {  
-  type = "zip"  
-  source_file = "${path.module}/lambda-src/hello-lambda.py" 
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "${path.module}/lambda-src/hello-lambda.py"
   output_path = "hello-lambda.zip"
 }
 
-data "aws_iam_policy_document" "lambda_assume_role_policy"{
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]    
-    principals {
-      type        = "Service"
-      identifiers = ["*"]
-    }
-  }
+# data "aws_iam_policy_document" "lambda_assume_role_policy" {
+#   # statement {
+#   #   effect  = "Allow"
+#   #   actions = ["sts:AssumeRole"]
+#   #   principals {
+#   #     type        = "Service"
+#   #     identifiers = ["lambda.amazonaws.com"]
+#   #   }
+#   # }
 
-  statement {
-    effect = "Allow"
-    actions = [
-       "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-    ]
-    principals {
-      type = "Service"
-      identifiers = [ "*" ]
-    }
-  }
-}
 
-resource "aws_iam_role" "lambda_role" {  
-  name = "lambda-lambdaRole-waf"  
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
-}
+#   statement {
+#     sid     = "logsLambda"
+#     effect  = "Allow"
+#     actions = ["logs:*"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
+#     }
+#     # resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_role" "lambda_role" {
+#   name               = "lambda-lambdaRole-waf"
+#   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+# }
 
 resource "aws_lambda_function" "hello_lambda" {
-  function_name = "hello_lambda_func"
-  filename      = "hello-lambda.zip"
+  function_name    = "hello_lambda_func"
+  filename         = "hello-lambda.zip"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "python3.6"
-  handler       = "hello-lambda.lambda_handler"
-  timeout       = 10
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = "python3.6"
+  handler          = "hello-lambda.lambda_handler"
+  timeout          = 10
 }
 
 resource "aws_cloudwatch_log_group" "example" {
   name              = "/aws/lambda/hello_lambda_func"
   retention_in_days = 14
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambdaRoleIAM"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "stsLambdaSid",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+    
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy_logs" {
+  name = "lambdaLogsPolicy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "iam_for_lambda_attach_policy_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.policy_logs.arn
 }
